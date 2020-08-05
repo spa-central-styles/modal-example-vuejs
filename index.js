b90157f3be760865f7cb6a7d4a12b447e15530c8
spa-central-styles <ui@multiplan.com>
2020-08-05 13:38:35 -0500

    1 file modified

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const vue = new Vue({
   el: '#app',
   data: {
     title: `Vue JS Template!
-    sd`
+    sd`,
   },
   template:`<div>
 
-  <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')">Launch demo modal</b-button>
+  <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')">Alert Modal</b-button>
   
   <b-modal id="bv-modal-example" hide-footer>
-    <template v-slot:modal-title class="modal-header p-0">
+    <template v-slot:modal-title class="h6 modal-header p-0">
       <alert show ><i class="fa fa-fw fa-exclamation-triangle"></i>Claims must be from a single Payor for comment</alert>
     </template>
     <div class="modal-body">
@@ -34,7 +34,16 @@ const vue = new Vue({
     </div>
   </b-modal>
 
+  <b-button v-b-modal.modal-scrollable>Scrolling modal</b-button>
+
+  <b-modal id="modal-scrollable" scrollable title="Scrollable Content">
+    <p class="my-4" v-for="i in 20" :key="i">
+      Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+      in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+  </b-modal>
   
+
 </div>`
 });
 
